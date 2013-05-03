753174e0c5e14efdd659f19ce298167cc652ac86
Jim Munro <jim.munro@sendgrid.com>
2013-05-03 16:12:05 -0600

    using customized invoices app

--- myinvoices/urls.py
+++ myinvoices/urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls import patterns, include, url
-from .views import MyInvoiceListView, MyInvoiceDetailView, full_invoice
+from invoices.views import InvoiceListView, InvoiceDetailView, full_invoice
 
 urlpatterns = patterns('',
 
 	url(r'^$', 
-	    	MyInvoiceListView.as_view(
+	    	InvoiceListView.as_view(
 	    		template_name="invoices/invoice_list.html"),
 	    	name="invoice_list"),
 	url(r'^(?P<pk>\d+)/$',
-		MyInvoiceDetailView.as_view(
+		InvoiceDetailView.as_view(
 			template_name="invoices/invoice_detail.html"),
 		name="invoice_detail"),
 
